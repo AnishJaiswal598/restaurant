e18e6c406d47b2d4716966c93c1620223b827506
AnishJaiswal598 <anishjaiswal0987@gmail.com>
2025-04-09 19:25:43 +0530

    footer: remove contact component from navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { TeamComponent } from './pages/team/team.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ContactComponent } from './pages/home/contact/contact.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'team', component: TeamComponent },
-  { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // default route
 ];
