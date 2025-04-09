0e5d9312b6005b8477b3be20ae545ee45d17fc78
anishjaiswal0987 <anishjaiswal0987@gmail.com>
2025-04-09 15:38:16 +0530

    homoe-page: update routes for entire project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContactComponent } from './pages/home-page/contact/contact.component';
-import { NavBarComponent } from './pages/home-page/nav-bar/nav-bar.component';
+import { ContactComponent } from './pages/home/contact/contact.component';
+import { NavBarComponent } from './pages/home/nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,5 +12,14 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  imageBG: boolean = true;
   title = 'chinese-resturant';
+
+  setImageBG = (tab: string) => {
+    if (tab != 'home') {
+      this.imageBG = false;
+    } else {
+      this.imageBG = true;
+    }
+  };
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { TeamComponent } from './pages/team/team.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // default route
+];
