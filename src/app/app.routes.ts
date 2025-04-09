07b756705d91b903dd7ecbbb31c48455a4ca3f5f
AnishJaiswal598 <anishjaiswal0987@gmail.com>
2025-04-09 18:52:58 +0530

    footer: complete and add footer component to all the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { TeamComponent } from './pages/team/team.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/home/contact/contact.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
