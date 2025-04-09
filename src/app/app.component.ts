90c353a12808babbe3b5b0fa7b3f94822eff2fcb
anishjaiswal0987 <anishjaiswal0987@gmail.com>
2025-04-09 14:43:09 +0530

    home-page: add home-page logic in app component for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ContactComponent } from './pages/home-page/contact/contact.component';
 import { NavBarComponent } from './pages/home-page/nav-bar/nav-bar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent],
+  imports: [RouterOutlet, ContactComponent, NavBarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
