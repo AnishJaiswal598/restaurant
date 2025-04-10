4d5c9cd6f92bbffacfbaf9390f3521274a50874e
AnishJaiswal598 <anishjaiswal0987@gmail.com>
2025-04-10 20:09:28 +0530

    menu: updated menu types css

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { FoodListComponent } from './food-list/food-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [FoodListComponent],
+  imports: [FoodListComponent, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  selectType: string = 'all';
+  changeType = (type: string) => {
+    this.selectType = type;
+  };
+}
