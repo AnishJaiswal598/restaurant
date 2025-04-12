8e934dfe21c2567b8abb8e3fa696a9d31d7e9ccf
AnishJaiswal598 <anishjaiswal0987@gmail.com>
2025-04-13 01:50:04 +0530

    feature/booking: add scrollable date section

--- src/app/pages/book-table/book-table.component.ts
+++ src/app/pages/book-table/book-table.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { dayDate } from '../../interfaces/dayDate.interface';
 
 @Component({
   selector: 'app-book-table',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './book-table.component.html',
-  styleUrl: './book-table.component.css'
+  styleUrl: './book-table.component.css',
 })
-export class BookTableComponent {
+export class BookTableComponent implements OnInit {
+  dateList: dayDate[] = [];
+  start: number = 0;
+  ngOnInit(): void {
+    this.dateList.push({ dayType: 'Today', Date: new Date().getDate() });
+    for (let i = 1; i < 30; i++) {
+      this.dateList.push({
+        dayType: new Date(Date.now() + i * 86400000)
+          .toLocaleString('default', {
+            weekday: 'long',
+          })
+          .substring(0, 3),
+        Date: new Date(Date.now() + i * 86400000).getDate(),
+      });
+    }
+  }
 
+  getYear = () => {
+    return new Date(Date.now() + 86400000).getFullYear();
+  };
+
+  getMonth = () => {
+    return new Date(Date.now() + 86400000).toLocaleString('default', {
+      month: 'long',
+    });
+  };
+
+  visibleDates = () => {
+    return this.dateList.slice(this.start, this.start + 8);
+  };
+
+  goLeft = () => {
+    if (this.start > 0) {
+      this.start -= 1;
+    }
+  };
+
+  goRight = () => {
+    if (this.start + 8 < 30) {
+      this.start += 1;
+    }
+  };
+
+  onScroll = (event: WheelEvent) => {
+    if (event.deltaY > 0) {
+      this.goRight();
+    } else {
+      this.goLeft();
+    }
+  };
 }
