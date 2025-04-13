7bbfc1089582122332a35055d7dc4460e478186e
AnishJaiswal598 <anishjaiswal0987@gmail.com>
2025-04-14 00:20:15 +0530

    booking: add directive for highllighting tables

---
+++ src/app/directives/table-highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TableHighlightDirective } from './table-highlight.directive';
+
+describe('TableHighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TableHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
