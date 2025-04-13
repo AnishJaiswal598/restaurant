08aeaac0b8f182522ca8a98b41e19b2ad8ef4144
AnishJaiswal598 <anishjaiswal0987@gmail.com>
2025-04-13 14:08:39 +0530

    booking: create basic time section

--- src/app/pages/book-table/time-picker/time-picker.component.ts
+++ src/app/pages/book-table/time-picker/time-picker.component.ts
@@ -5,8 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './time-picker.component.html',
-  styleUrl: './time-picker.component.css'
+  styleUrl: './time-picker.component.css',
 })
 export class TimePickerComponent {
-
+  morningSlots: string[] = ['9:00 - 10:00', '10:00 - 11:00', '11:00 - 12:00'];
+  afternoonSlots: string[] = [
+    '12:00 - 1:00',
+    '1:00 - 2:00',
+    '2:00 - 3:00',
+    '3:00 - 4:00',
+  ];
+  eveningSlots: string[] = [
+    '6:00 - 7:00',
+    '7:00 - 8:00',
+    '8:00 - 9:00',
+    '9:00 - 10:00',
+    '10:00 - 11:00',
+    '11:00 - 12:00',
+  ];
 }
