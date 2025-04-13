553b3a5a6b9aa506be8be0a84a84de1218bddbaa
AnishJaiswal598 <anishjaiswal0987@gmail.com>
2025-04-13 14:08:39 +0530

    booking: create basic time section

--- src/app/pages/book-table/book-table.component.ts
+++ src/app/pages/book-table/book-table.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DatePickerComponent } from './date-picker/date-picker.component';
+import { TimePickerComponent } from './time-picker/time-picker.component';
 
 @Component({
   selector: 'app-book-table',
   standalone: true,
-  imports: [DatePickerComponent],
+  imports: [DatePickerComponent, TimePickerComponent],
   templateUrl: './book-table.component.html',
   styleUrl: './book-table.component.css',
 })
