87917abc1bf2ea1a956ced9dfc38a72384c13b3d
AnishJaiswal598 <anishjaiswal0987@gmail.com>
2025-04-15 23:38:05 +0530

    cart: show an empty component if the cart is not filled

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -3,11 +3,17 @@ import { CartItemService } from '../../services/cart-item.service';
 import { CartListComponent } from './cart-list/cart-list.component';
 import { CartSummaryComponent } from './cart-summary/cart-summary.component';
 import { cartItem } from '../../interfaces/cartItem.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CartListComponent, CartSummaryComponent, CartListComponent],
+  imports: [
+    CartListComponent,
+    CartSummaryComponent,
+    CartListComponent,
+    RouterLink,
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
